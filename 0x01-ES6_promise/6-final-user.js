33cec773a8d97ec99c5735329dcd663c9d35441a
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-01-03 22:47:26 +0000

    JS

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,14 +1,25 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ];
+export function handleProfileSignup(firstName, lastName, fileName) {
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
 
-  return Promise.allSettled(promises).then((results) => results.map((result) => ({
-    status: result.status,
-    value: result.status === 'fulfilled' ? result.value.body : result.reason.message,
-  })));
+  return Promise.allSettled([userPromise, photoPromise]).then((results) => {
+    return results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return {
+          status: result.status,
+          value: result.value,
+        };
+      } else {
+        return {
+          status: result.status,
+          value: result.reason,
+        };
+      }
+    });
+  });
 }
+
+export default handleProfileSignup;
