dbfe9bcf580e846278792e9c5016220ee8ea4966
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-01-03 17:46:49 +0000

    JS

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,13 +1,13 @@
 function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      const apiResponse = "Sample API response";
+      const apiResponse = 'Sample API response';
       const isAPISuccessful = true;
 
       if (isAPISuccessful) {
         resolve(apiResponse);
       } else {
-        reject(new Error("API call failed"));
+        reject(new Error('API call failed'));
       }
     }, 1000);
   });
