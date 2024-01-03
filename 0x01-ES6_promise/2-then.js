58a8e571ecfd28f8ae0d6531ea10477e7ea9e780
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-01-03 18:14:48 +0000

    JS

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,13 @@
+function handleResponseFromAPI(promise) {
+  return promise
+    .then((response) => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'success' };
+    })
+    .catch((error) => {
+      console.error(error);
+      return new Error();
+    });
+}
+
+export default handleResponseFromAPI;
