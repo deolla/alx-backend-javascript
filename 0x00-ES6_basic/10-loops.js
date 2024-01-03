5b74df2ae6b9051c6f9e9ef3967b97328d22b31c
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-01-03 13:30:33 +0000

    JS

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,5 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (const [idx, value] of array.entries()) {
+  for (let value of array) {
+    let idx = array.indexOf(value);
     array[idx] = appendString + value;
   }
 
