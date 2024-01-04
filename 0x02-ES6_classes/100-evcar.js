c1132944e0357870a1c96b926cd93a9b85406e73
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-01-04 15:03:53 +0000

    JS

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,11 +1,13 @@
 import Car from './10-car';
 
 export default class EvCar extends Car {
-    constructor(brand, motor, color, range) {
-        super(brand, motor, color);
-        this._range = range;
-    }
-    cloneCar() {
-        return new Car();
-    }
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  cloneCar() {
+    return new Car();
+  }
 }
