0a4dcfc9f7dc75f7590dd79d0ce0eae56d5991a0
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-01-04 14:07:07 +0000

    JS

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,15 +1,12 @@
 export default class Building {
-    evacuationWarningMessage() {
-        throw new Error('Class extending Building must override evacuationWarningMessage');
+  constructor(sqft) {
+    if (this.constructor !== Building && this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
-    constructor(sqft) {
-        if (this.constructor !== Building && this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
-            throw new Error('Class extending Building must override evacuationWarningMessage');
-        }
-        this._sqft = sqft;
-    }
+    this._sqft = sqft;
+  }
 
-    get sqft() {
-        return this._sqft;
-    }
+  get sqft() {
+    return this._sqft;
+  }
 }
