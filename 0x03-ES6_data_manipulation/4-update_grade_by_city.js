bdcb51224a739f83c517453891e3330e9ac2ae06
root <root@Adeola>
2024-01-08 14:30:31 +0100

    JS

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,12 +1,12 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  /* return students.filter((students) => students.location === city)
-        .map((student) => {
-            const grades = newGrades.filter((grade) => grade.studentId === student.id);
-            return {
-                ...student,
-                grade: grades.length > 0 ? grades[0].grade : 'N/A',
-            }
-        }); */
+/* return students.filter((student) => student.location === city)
+    .map((student) => {
+    const grades = newGrades.filter((grade) => grade.studentId === student.id);
+    return {
+        ...student,
+        grade: grades.length > 0 ? grades[0].grade : 'N/A',
+    }
+  }); */
   return students.filter((student) => student.location === city).map((student) => {
     const grade = newGrades.filter((grade) => grade.studentId === student.id)[0];
     return {
