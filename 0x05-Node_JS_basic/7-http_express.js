cdd9773d31e845ed4065af7f5b162fd2a79719bc
Adeosun <deosundeola@gmail.com>
2024-02-27 19:40:56 +0000

    changes

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -51,7 +51,7 @@ app.get('/', (req, res) => {
 app.get('/students', (req, res) => {
   countStudents(process.argv[2].toString())
     .then((output) => {
-      res.send(['This is the list of our students', output].join("\n"));
+      res.send(['This is the list of our students', output].join('\n'));
     })
     .catch(() => {
       res.send('This is the list of our students\nCannot load the database');
